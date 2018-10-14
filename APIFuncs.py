db3c7cc64c1dbed67cc902c41701a3826b9ef7ee
Your Name <remindforzhang@163.com>
2018-10-14 18:21:05 +0800

    2018-10-8-4

--- APIFuncs.py
+++ APIFuncs.py
@@ -26,35 +26,46 @@ def query_users():
     return users2,columns
 
 users,cols = query_users()
-print(users)
-print(cols)
+
 usersDF = pd.DataFrame(users,columns=cols)
-print(usersDF)
+usersDF['分数'] = usersDF['分数'].astype('int')
 # -----------------------------------------------------------------------------------------
 # 统计分析都基于dataframtest做
 # -----------------------------------------------------------------------------------------
 # value_counts 统计某列的次数
-print(usersDF.head(5))
 # print(usersDF.age.value_counts())
-print(usersDF['年龄'].value_counts())
+# print(usersDF['年龄'].value_counts())
+# print(usersDF.groupby(['年龄'],sort=True))
 # groupby 函数分类
 # print(usersDF['星座：'].groupby(usersDF['age']).value_counts())
 # print(usersDF['星座：'].groupby([usersDF['age'],usersDF['血型：']]).value_counts())
 # # agg函数
 # print(usersDF['age'].groupby(usersDF['星座：']).agg(['max','min']))
 # apply() 将函数递归运用到一列上
-def test_return(age):
-    if 18 < int(age) < 25:
-        return 1
-    if 25 <= int(age) < 35:
-        return 0
-print(usersDF['age'].apply(test_return))
+# def test_return(age):
+#     if 18 < int(age) < 25:
+#         return 1
+#     if 25 <= int(age) < 35:
+#         return 0
+# print(usersDF['age'].apply(test_return))
 
 
 # --------------------------------------------------------------------------------------------------
 #### 需要向外expose成api的func #####
 # --------------------------------------------------------------------------------------------------
-# # 功能二 根据某一项查询出对应的用户的某项信息的统计
-# def get_single_attr_by_single_attr():
-#
-#     pass
+# 根据分数段返回这一段的用户的各项指标的统计信息
+def get_analyis_data_by_score(SBeign,SEnd,attr):
+    usersAS = usersDF[usersDF['分数'] > SBeign][usersDF['分数'] < SEnd]
+    userASHeight = usersAS[attr].value_counts()
+    sss = usersAS[attr].sum()
+    dddd = pd.DataFrame(userASHeight)
+    dddd['频率'] = dddd/dddd[attr].sum()
+    print(dddd)
+    return dddd.to_dict()
+# ----------------------------------------------------------------------------------------------------
+# example
+# ----------------------------------------------------------------------------------------------------
+usersR = get_analyis_data_by_score( 50,70,'身高')
+print(usersR)
+usersR = get_analyis_data_by_score( 50,70,'住房')
+print(usersR)
